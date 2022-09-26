e72c612eb9f875885f0cee0f4517f56251d41a51
necipfazilgocer <114177524+necipfazilgocer@users.noreply.github.com>
2022-09-27 00:15:54 +0300

    Add files via upload

---
+++ asal.java
@@ -0,0 +1,19 @@
+package asal;
+
+public class asal {
+
+	public static void main(String[] args) {
+	System.out.print("2\n"+"3\n"+"5\n"+"7\n"+"11\n");
+		for(int i=2;i<=100;i++)
+		{
+		if(i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0 && i%11!=0 ) {
+			
+			System.out.println(i);
+		}
+		
+		
+
+	}
+
+}
+}
